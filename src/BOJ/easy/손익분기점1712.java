55f59e65491c63efa7e207178cb8f186140ae4d7
JeongInjin <snowwwwww@naver.com>
2021-11-04 21:45:04 +0900

    문제

---
+++ src/BOJ/easy/손익분기점1712.java
@@ -0,0 +1,23 @@
+package BOJ.easy;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 손익분기점1712 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken()); // 고정지출
+        int B = Integer.parseInt(st.nextToken()); // 원가
+        int C = Integer.parseInt(st.nextToken()); // 판매가
+
+        if(C <= B) System.out.print("-1");
+        else System.out.print(A/(C-B) + 1);
+    }
+
+}
