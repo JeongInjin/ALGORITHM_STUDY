e4d926befcd9c4eb88502cb9100c8766a20be4b3
JeongInjin <snowwwwww@naver.com>
2021-11-05 21:44:01 +0900

    문제

---
+++ src/BOJ/easy/AB1001.java
@@ -0,0 +1,29 @@
+package BOJ.easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+//public class AB1001 {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//
+//        System.out.print(A-B);
+//        sc.close();
+//    }
+//}
+
+public class AB1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.print(A-B);
+    }
+}
