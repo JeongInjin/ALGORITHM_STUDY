3bfa86ee4fd185004a576e6be495d6c3e5c21299
JeongInjin <snowwwwww@naver.com>
2021-11-18 22:09:03 +0900

    문제를 풀어보자!!

---
+++ src/infcode/secssion4_HashMap_HashSet_TreeSet/quiz_4_5_Main.java
@@ -0,0 +1,38 @@
+package infcode.secssion4_HashMap_HashSet_TreeSet;
+
+import java.util.*;
+
+public class quiz_4_5_Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solution(arr, n, k));
+    }
+    public static int solution(int[] arr, int n, int k){
+        int answer=-1;
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                for(int l=j+1; l<n; l++){
+                    Tset.add(arr[i]+arr[j]+arr[l]);
+                }
+            }
+        }
+        int cnt=0;
+        //Tset.remove(143);
+        //System.out.println(Tset.size());
+        //System.out.println("first : "+ Tset.first());
+        //System.out.println("last : "+ Tset.last());
+
+        for(int x : Tset){
+            cnt++;
+            if(cnt==k) return x;
+        }
+        return answer;
+    }
+}
