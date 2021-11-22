b6d21d3ce644af52a36c0a5ba1857dc578e90c32
jeonginjin <snowwwwww@naver.com>
2021-11-22 20:11:08 +0900

    quiz문제를 풀어보자.

--- src/infcode/secssion7_Recursive_Tree_Graph/quiz_7_8_Breadth_First_Search_송아지찾기.java
+++ src/infcode/secssion7_Recursive_Tree_Graph/quiz_7_8_Breadth_First_Search_송아지찾기.java
@@ -2,53 +2,63 @@ package infcode.secssion7_Recursive_Tree_Graph;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 /*
-이진트리 순회(넓이우선탐색 : 레벨탐색) BFS : Breadth-First-Search
-아래 그림과 같은 이진트리를 레벨탐색 연습하세요.
-    1
-   2 3
- 4 5 6 7
+8. 송아지 찾기 1(BFS : 상태트리탐색)
+설명
+현수는 송아지를 잃어버렸다. 다행히 송아지에는 위치추적기가 달려 있다.
+현수의 위치와 송아지의 위치가 수직선상의 좌표 점으로 주어지면 현수는 현재 위치에서 송아지의 위치까지 다음과 같은 방법으로 이동한다.
+송아지는 움직이지 않고 제자리에 있다.
+현수는 스카이 콩콩을 타고 가는데 한 번의 점프로 앞으로 1, 뒤로 1, 앞으로 5를 이동할 수 있다.
+최소 몇 번의 점프로 현수가 송아지의 위치까지 갈 수 있는지 구하는 프로그램을 작성하세요.
 
-레벨 탐색 순회 출력 : 1 2 3 4 5 6 7
+입력
+첫 번째 줄에 현수의 위치 S와 송아지의 위치 E가 주어진다. 직선의 좌표 점은 1부터 10,000까지이다.
+
+출력
+점프의 최소횟수를 구한다. 답은 1이상이며 반드시 존재합니다.
+
+예시 입력 1
+5 14
+예시 출력 1
+3
 */
 public class quiz_7_8_Breadth_First_Search_송아지찾기 {
-    static class Node{
-        int data;
-        Node lt, rt;
-        public Node(int val) {
-            data=val;
-            lt=rt=null;
-        }
-    }
-    private Node root;
-
+    static int answer=0;
+    static int[] dis={1, -1, 5};
+    static int[] ch;
+    static Queue<Integer> Q = new LinkedList<>();
     public static void main(String[] args){
-        quiz_7_8_Breadth_First_Search_송아지찾기 tree = new quiz_7_8_Breadth_First_Search_송아지찾기();
-        tree.root = new Node(1);
-        tree.root.lt = new Node(2);
-        tree.root.rt = new Node(3);
-        tree.root.lt.lt = new Node(4);
-        tree.root.lt.rt = new Node(5);
-        tree.root.rt.lt = new Node(6);
-        tree.root.rt.rt = new Node(7);
-        tree.BFS(tree.root);
+        Scanner kb = new Scanner(System.in);
+        int s=kb.nextInt();
+        int e=kb.nextInt();
+        System.out.println(BFS(s, e));
     }
-    public static void BFS(Node root){
-        Queue<Node> Q = new LinkedList<>();
-        Q.add(root);
-        int L = 0;
+    public static int BFS(int s, int e){
+        ch=new int[10001];
+        ch[s]=1;
+        Q.offer(s);
+        int L=0;
         while(!Q.isEmpty()){
-            int len = Q.size();
-            System.out.print(L + " : ");
-            for(int i = 0; i < len; i++){
-                Node cur = Q.poll();
-                System.out.print(cur.data + " ");
-                if(cur.lt != null) Q.add(cur.lt);
-                if(cur.rt != null) Q.add(cur.rt);
+            int len=Q.size();
+            for(int i=0; i<len; i++){
+                int x = Q.poll();
+                for(int j=0; j<3; j++){
+                    int nx=x+dis[j];
+                    if(nx==e){
+                        return L+1;
+                    }
+                    if(nx>=1 && nx<=10000 && ch[nx]==0){
+                        ch[nx]=1;
+                        Q.offer(nx);
+                    }
+                }
             }
             L++;
-            System.out.println();
         }
+        return 0;
     }
+
+
 }
---
+++ src/infcode/test/test_1.java
@@ -0,0 +1,41 @@
+package infcode.test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class test_1 {
+    static int[] dis = {1, -1, 5};
+    static int[] ch;
+    static Queue<Integer> Q = new LinkedList<>();
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int start = kb.nextInt();
+        int end = kb.nextInt();
+
+
+        System.out.println(BFS(start, end));
+    }
+    public static int BFS(int s, int e){
+        ch = new int[10001];
+        ch[s] = 1;
+        Q.offer(s);
+        int L = 0;
+        while(!Q.isEmpty()){
+            int len = Q.size();
+            for(int i = 0; i < len; i++){
+              int x = Q.poll();
+              for(int k =0; k < 3; k++){
+                  int nx = x + dis[k];
+                  if(nx == e) return L + 1;
+                  if(nx >= 1 && nx <= 10000&& ch[nx] == 0){
+                      ch[nx] = 1;
+                      Q.offer(nx);
+                  }
+              }
+            }
+            L++;
+        }
+        return 0;
+    }
+}
