f9065d0d97133e661dc5f67aae57246fe61bc88a
jeonginjin <snowwwwww@naver.com>
2021-12-01 14:38:53 +0900

    문제를 풀어보자!

---
+++ src/infcode/test/test22.java
@@ -0,0 +1,34 @@
+package infcode.test;
+
+import java.util.Scanner;
+
+public class test22 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(solution(n, t, arr));
+    }
+    public static int solution(int n, int t, int[] arr){
+        int answer = 0;
+        int sum =0;
+        int lt = 0;
+        int p2 =0;
+
+        for(int rt = 0; rt < n; rt++){
+            sum += arr[rt];
+            if(sum == t) answer++;
+
+            while(sum >= t){
+                sum -= arr[lt++];
+                if(sum == t) answer++;
+            }
+        }
+
+        return answer;
+    }
+}
