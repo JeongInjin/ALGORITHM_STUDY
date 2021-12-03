7f611039af6ab11f17b2747231f7d8fe53fbd7bb
JeongInjin <snowwwwww@naver.com>
2021-12-03 23:09:31 +0900

    문제를 풀어보자!!

---
+++ src/LeetCode/Easy/easy234_PalindromeLinkedList.java
@@ -0,0 +1,33 @@
+package LeetCode.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class easy234_PalindromeLinkedList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public boolean isPalindrome(ListNode head) {
+            if(head == null) return false;
+
+            List<Integer> list = new ArrayList<>();
+
+            while(head != null){
+                list.add(head.val);
+                head = head.next;
+            }
+            for(int i = 0; i < list.size() / 2; i++){
+                if(list.get(i) != list.get(list.size() - i - 1)) return false;
+            }
+            return true;
+        }
+    }
+}
