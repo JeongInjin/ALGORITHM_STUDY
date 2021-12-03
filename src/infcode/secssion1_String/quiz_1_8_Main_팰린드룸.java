76d9fe07891abffeca885e63451c3395a8114560
JeongInjin <snowwwwww@naver.com>
2021-12-03 14:50:39 +0900

    문제를 풀어보자!!

--- src/infcode/secssion1_String/quiz_1_8_Main_팰린드룸.java
+++ src/infcode/secssion1_String/quiz_1_8_Main_팰린드룸.java
@@ -11,14 +11,14 @@ public class quiz_1_8_Main_팰린드룸 {
         System.out.print(solution(str));
     }
     public static String solution(String str){
-        String answer = "NO";
+        String answer = "YES";
         //String compare = String.valueOf(new StringBuilder(str).reverse()).toUpperCase().replaceAll("[^A-Z]","");
         //if(str.toUpperCase().replaceAll("[^A-Z]", "").equals(compare)) answer = "YES";
 
         //문자열로 넘어온 팰린더룸 체크 for를 이용함
         for(int i = 0; i < str.length() / 2; i++){
             if(str.charAt(i) != str.charAt(str.length() - i - 1))
-                return answer;
+                return "NO";
         }
 
         return answer;
