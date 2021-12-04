ccd6a6c51f5ff489073d8e9fe97120d005f9db90
JeongInjin <snowwwwww@naver.com>
2021-12-04 15:07:35 +0900

    문제를 풀어보자!!

---
+++ src/LeetCode/Easy/easy448_FindAllNumbersDisappearedinanArray.java
@@ -0,0 +1,43 @@
+package LeetCode.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+
+ */
+public class easy448_FindAllNumbersDisappearedinanArray {
+    class Solution {
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            int[] ch = new int[nums.length + 1];
+            List<Integer> answer = new ArrayList<>();
+
+            for(int i = 0; i < nums.length; i++){
+                ch[nums[i]] = 1;
+            }
+            for(int i = 1; i< ch.length; i++){
+                if(ch[i] == 0) answer.add(i);
+            }
+            return answer;
+        }
+    }
+}
