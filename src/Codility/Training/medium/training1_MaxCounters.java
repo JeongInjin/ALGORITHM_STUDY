6997bb36bf6528d6c74a2c7b0c8e174cc593125e
jeonginjin <snowwwwww@naver.com>
2021-12-08 10:45:17 +0900

    문제를 풀어보자!

--- src/Codility/Training/medium/training1_MaxCounters.java
+++ src/Codility/Training/medium/training1_MaxCounters.java
@@ -26,12 +26,37 @@ class Solution {
 }
  */
 public class training1_MaxCounters {
+//score 100% 나옴 valueMax를 찾았을시 fill함수로 업데이트를 일괄로 하는게아니라 추후 찾아서 for 문을 한번 더 돌림
+    class Solution {
+        public int[] solution(int N, int[] A) {
+            int[] arr = new int[N];
+            int valueMax = 0;
+            int tmp = 0;
 
+            for(int i = 0; i < A.length; i++){
+                //System.out.println(A[i]);
+                if(A[i] > N){
+                    //System.out.println("valueMax : " + valueMax);
+                    //Arrays.fill(arr, valueMax);
+                    valueMax = tmp;
+                }else{
+                    //System.out.println("A[i] - 1 : " + (A[i] - 1));
+                    arr[A[i] - 1] = Math.max(valueMax, arr[A[i]-1]) + 1; // 1씩 증가
+                    if(arr[A[i] - 1] > tmp) tmp = arr[A[i] - 1];
+                }
+            }
+            for (int i = 0; i < N; i++) {
+                if(arr[i] < valueMax) arr[i] = valueMax;
+            }
+
+
+            return arr;
+        }
+    }
 
     //아래는 score 77% 나옴..
     //정답은 전부 맞으나 랜덤숫자로 뽑은 긴 문자열은 타입아웃이 떨어짐..
-    //O(n)이 맞는거같은데..효율적인 방법을 찾아봐야겠음..
-
+    /*
     class Solution {
         public int[] solution(int N, int[] A) {
             int[] arr = new int[N];
@@ -52,4 +77,5 @@ public class training1_MaxCounters {
             return arr;
         }
     }
+    */
 }
