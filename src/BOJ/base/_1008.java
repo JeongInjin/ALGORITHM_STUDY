0c898dbff0afa35a68b0288f85d61f2f3f5eb748
injinjeong <snowwwwww@naver.com>
2023-01-22 11:25:40 +0900

    base

---
+++ src/BOJ/base/_1008.java
@@ -0,0 +1,16 @@
+package BOJ.base;
+
+import java.util.Scanner;
+
+public class _1008 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long A = in.nextLong();
+        long B = in.nextLong();
+        long C = in.nextLong();
+
+        System.out.println(A + B + C);
+
+        in.close();
+    }
+}
