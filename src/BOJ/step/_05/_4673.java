88a5456ae98ed8367607825ccd6b9d32b0123c21
injinjeong <snowwwwww@naver.com>
2023-02-10 16:17:10 +0900

    arg

---
+++ src/BOJ/step/_05/_4673.java
@@ -0,0 +1,60 @@
+package BOJ.step._05;
+
+import java.io.IOException;
+
+public class _4673 {
+    public static void main(String[] args) throws IOException {
+        boolean[] check = new boolean[10001];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 10001l; i++) {
+            int num = i;
+            int sum = num;
+            while (num != 0) {
+                sum = sum + (num % 10);
+                num = num / 10;
+            }
+            if (sum < 10001) {
+                check[sum] = true;
+            }
+        }
+        for (int i = 1; i < 10001; i++) {
+            if (!check[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+//    public static void main(String[] args) {
+//
+//        boolean[] check = new boolean[10001];
+//
+//        for (int i = 1; i < 10001; i++) {
+//            int n = d(i);
+//            if (n < 10001) {
+//                check[n] = true;
+//            }
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 1; i < 10001; i++) {
+//            if (!check[i]) {
+//                sb.append(i).append('\n');
+//            }
+//        }
+//        System.out.println(sb);
+//    }
+//
+//
+//    public static int d(int number) {
+//        int sum = number;
+//
+//        while (number != 0) {
+//            sum = sum + (number % 10);
+//            number = number / 10;
+//        }
+//
+//        return sum;
+//    }
+}
